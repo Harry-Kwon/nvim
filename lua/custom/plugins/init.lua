78a5abea2d51794495a8eed5bf0f9922bac3cdd5
Harry Kwon <harry@harry-ubuntu.lan>
2023-04-09 23:53:20 -0400

    add neo-tree and keymaps

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.o.autoindent = true
+vim.o.ruler = true
+vim.o.number = true
+
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+
 return {}
